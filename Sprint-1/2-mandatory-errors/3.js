41c163769297a9d5c325940f7a27f584f54e7075
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2025-11-30 20:23:17 +0100

    Added comments to 3.js

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -7,3 +7,18 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+//The code will not work because cardNumber is a number, and numbers do not have the .slice() method.
+//.slice() only works on strings and arrays, not on numeric values.
+//So I expect the code to throw a TypeError, saying something like:"cardNumber.slice is not a function".
+//Actual error:TypeError: cardNumber.slice is not a function
+//the error matches the prediction.
+//The issue is exactly that .slice() cannot be used on a number.
+//The error happened because JavaScript numbers are primitive values, they do not have array or string methods. To use slice, the value must first be converted to a string.
+//We need to convert the number to a string before slicing:
+
+//const cardNumber = 4533787178994213;
+//const last4Digits = cardNumber.toString().slice(-4);
+
+//console.log(last4Digits); 
+//The result will be 4213
