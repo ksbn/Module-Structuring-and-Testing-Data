cafb7621a2d46fe55b27d8ea8dd066bd328b9be2
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2025-11-30 19:54:20 +0100

    Added comments to 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -3,3 +3,16 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+//The error:
+
+//We are trying to use the variable cityOfBirth before it is declared.
+//With const, the variable is not accessible before its declaration because of the temporal dead zone.So the code throws an error.
+//JavaScript reads the file from top to bottom, so cityOfBirth does not exist yet when the console.log line runs and const variables must be declared before we use them.
+//We need to move the declaration above the console.log
+
+//The corrected code:
+
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+//The result will be: I was born in Bolton
