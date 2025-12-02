19a4bd6830a6b009dacaf4e88bcdde28663047e7
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2025-12-02 19:18:57 +0100

    Added comments to 4.js

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,10 @@
 const 12HourClockTime = "20:53";
 const 24hourClockTime = "08:53";
+
+//This causes a syntax error because in JavaScript, a variable name cannot start with a number.Therefore, 12HourClockTime and 24HourClockTime are incorrect identifiers, and the interpreter reports errors:Variable declaration expectedAn identifier or keyword cannot immediately follow a numeric literal
+//We need to rename the variables so that they start with a letter:
+
+const hour12ClockTime = "20:53";
+const hour24ClockTime = "08:53";
+
+
