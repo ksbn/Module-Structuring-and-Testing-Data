f70f185aa0eb8af764a69b3a1f9872eed2ba3762
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2025-12-10 21:16:57 +0100

    Added comments to 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The function immediately returns without a value — this stops the function from running before a + b is executed.
+// Therefore: the function will return undefined
+// the string will be: The sum of 10 and 32 is undefined
 
 function sum(a, b) {
   return;
@@ -8,6 +10,23 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> 
+// The problem is that return stands alone:
+// return;
+// a + b;
+// JavaScript:
+// stops the function from executing at return
+// everything below (including a + b) is completely ignored
+// That is, a + b is never executed → the function always returns undefined.
+// This is a typical error associated with an empty return statement.
+
+
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> The correct version is to return the amount:
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); //The sum of 10 and 32 is 42
+
+
