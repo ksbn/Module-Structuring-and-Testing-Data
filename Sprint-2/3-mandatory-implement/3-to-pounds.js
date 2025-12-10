b1d07b4dfc1807ffa6013a16a43d27acde5de2c3
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2025-12-10 21:47:05 +0100

    Added comments to 3-to-pounds.js

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,19 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+// Function for converting kilograms to pounds
+function toPounds(kg) {
+    const pounds = kg * 2.20462; // 1 kg = 2.20462 pounds
+    return +pounds.toFixed(2);   // round to 2 decimal places
+}
+
+console.log(toPounds(10));   // 22.05
+console.log(toPounds(5));    // 11.02
+console.log(toPounds(70));   // 154.32
+console.log(toPounds(0));    // 0
+
+// The function takes one parameter, kg, which is the weight in kilograms.
+// We multiply by a factor of 2.20462 to convert to pounds.
+// We use toFixed(2) to round to two decimal places.
+// We return the result.
