23712e7622d8ab7fc3b1d8e1ea97c746bc50ce05
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2025-12-19 17:48:16 +0100

    Complete Sprint 3 practice and stretch tasks

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,6 +1,28 @@
-function passwordValidator(password) {
-    return password.length < 5 ? false : true
+// Array for storing previous passwords
+const previousPasswords = [];
+
+function isValidPassword(password, previousPasswords = []) {
+  // 1. minimum length of 5 characters
+  if (password.length < 5) return false;
+
+  // 2. There is at least one capital letter
+  if (!/[A-Z]/.test(password)) return false;
+
+  // 3. There is at least one lowercase letter
+  if (!/[a-z]/.test(password)) return false;
+
+  // 4. There is at least one number
+  if (!/[0-9]/.test(password)) return false;
+
+  // 5. There is at least one special character from the list
+  if (!/[!#$%.*&]/.test(password)) return false;
+
+  // 6. The password must not be repeated.
+  if (previousPasswords.includes(password)) return false;
+
+  // If all checks have been passed, add the password to the history and return it, then true
+  previousPasswords.push(password);
+  return true;
 }
 
-
-module.exports = passwordValidator;
+module.exports = isValidPassword;
